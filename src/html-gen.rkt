3cb0f9b99130192498285c0eeaf45171307737f4
Fernando94654 <A00841856@tec.mx>
2026-05-22 00:22:02 -0600

    change comments to english

--- src/html-gen.rkt
+++ src/html-gen.rkt
@@ -1,9 +1,10 @@
 #lang racket
 
 (provide tokenize-to-html)
+
 (require "lexer.rkt")
 
-; Escapes special HTML characters in a string
+; Escape special HTML characters in a string
 (define (html-encode str)
   (string-replace
    (string-replace
@@ -11,40 +12,45 @@
     "<" "&lt;")
    ">" "&gt;"))
 
-; Maps a token label to a CSS class
+; Map token label to a CSS class
 (define (label->class label)
-  (case label
-    [(kw_states kw_start kw_accepting kw_alphabet kw_transitions) "keyword"]
-    [(state_id)            "state-id"]
-    [(symbol_id)           "symbol-id"]
-    [(dos_puntos)          "dos-puntos"]
-    [(coma)                "coma"]
-    [(punto_coma)          "punto-coma"]
-    [(flecha)              "flecha"]
-    [(par_abre par_cierra) "parentesis"]
-    [(comment)             "comment"]
-    [else                  "unknown"]))
+  (cond
+    [(member label '("rw-automata" "rw-start" "rw-end"
+                     "rw-states" "rw-transitions" "rw-alphabet")) "keyword"]
+    [(equal? label "stateId")              "state-id"]
+    [(equal? label "alphabet-symbol")      "symbol-id"]
+    [(equal? label "dots")                 "dos-puntos"]
+    [(equal? label "transition-sybol")     "flecha"]
+    [(equal? label "coma")                 "coma"]
+    [(equal? label "semicol")              "punto-coma"]
+    [(member label '("right-straigth-parenthesis"
+                     "left-straigth-parenthesis")) "parentesis"]
+    [(equal? label "blank_space")          "blank"]
+    [(equal? label "identifier")           "identifier"]
+    [else                                  "unknown"]))
 
-; Generates the HTML <span> element for a token
+; Generate the HTML <span> element for a token
 (define (token->span tok)
-  (string-append "<span class=\"" (label->class (car tok)) "\">"
-                 (html-encode (cdr tok))
+  (string-append "<span class=\"" (label->class (first tok)) "\">"
+                 (html-encode (second tok))
                  "</span>"))
 
-; Generates the HTML snippet for a tokenized line (for inline insertion)
+; Generate HTML for a tokenized line (snippet for inline insertion)
 (define (line->html-snippet line-pair)
   (let ([tokens (cdr line-pair)])
     (if (null? tokens)
         ""
         (string-append (apply string-append (map token->span tokens)) "<br>\n"))))
 
-; ---- For the servlet: returns an HTML snippet from an input string ----
-(define (tokenize-to-html input-str)
-  (let* ([lines     (string-split input-str "\n")]
-         [tokenized (flatten-token lines)])
-    (apply string-append (map line->html-snippet tokenized))))
 
-; ---- For standalone use: generates a full HTML document ----
+; For servlet: return an HTML snippet for an input string
+(define (tokenize-to-html input-str)
+  (let* ([tokenized (Tokenizer input-str)]
+         [tokenized (first tokenized)]  ; take only the tokens part, discarding error-line
+         )
+    (apply string-append (map token->span tokenized))))
+
+; ---- For standalone: generate a complete HTML document ----
 (define html-styles
   (string-append
    "<style>\n"
@@ -68,3 +74,5 @@
    "</head>\n<body>\n<pre>\n"
    (apply string-append (map line->html-snippet tokenized-lines))
    "</pre>\n</body>\n</html>"))
+
+
