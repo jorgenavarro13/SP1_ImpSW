fd742dd9b468f057ed2261fe7c87fd722e3a228b
JorgeNavarro <jorge.85navarro@gmail.com>
2026-05-24 19:33:15 -0600

    Parser with recursive descent

--- src/parser.rkt
+++ src/parser.rkt
@@ -1,71 +1,394 @@
 #lang racket
 ; Parser: converts a flat token list into an automaton data structure
 
-(provide parse-transitions
-         parse-tokens)
-
-(define (parse-transitions flat-tokens)
-  ; First, isolate only the tokens inside transitions: [...]
-  ; Walk the list looking for groups of: stateId : symbol :: stateId
-
-  (define (parse-transitions-aux tokens acc)
-    (cond
-      ; BASE CASE: less than 5 tokens left, nothing more to parse
-      [(< (length tokens) 5) acc]
-
-      ; MATCH: found a transition pattern q0:0::q1
-      [(and (equal? (first (first tokens))  "stateId")           ; from-state
-            (equal? (first (second tokens)) "dots")               ; :
-            (equal? (first (third tokens))  "alphabet-symbol")    ; symbol
-            (equal? (first (fourth tokens)) "transition-sybol")  ; :: typo in original label
-            (equal? (first (fifth tokens))  "stateId"))           ; to-state
-
-       (let* ([from   (second (first tokens))]
-              [symbol (second (third tokens))]
-              [to     (second (fifth tokens))]
-              [new-transition (list from symbol to)])
-         ; Consume 5 tokens, add transition, recurse
-         (parse-transitions-aux (list-tail tokens 5)
-                                (append acc (list new-transition))))
-      ]
-
-      ; NO MATCH: skip one token and keep looking
-      [else (parse-transitions-aux (cdr tokens) acc)]
-    )
+(define raw-tokens
+  '(
+      ("rw-automata"  "Automaton")
+      ("blank_space"  " ")
+      ("identifier"  "dfa")
+      ("blank_space"  " ")
+      ("right-straigth-parenthesis"  "[")
+      ("newline"  "\n")
+      ("blank_space"  "  ")
+      ("rw-states"  "states")
+      ("blank_space"  " ")
+      ("dots"  ":")
+      ("right-straigth-parenthesis"  "[")
+      ("blank_space"  " ")
+      ("stateId"  "q0")
+      ("coma"  ",")
+      ("blank_space"  " ")
+      ("stateId"  "q1")
+      ("coma"  ",")
+      ("blank_space"  " ")
+      ("stateId"  "q2")
+      ("blank_space"  " ")
+      ("left-straigth-parenthesis"  "]")
+      ("newline"  "\n")
+      ("blank_space"  "  ")
+      ("rw-alphabet"  "alphabet")
+      ("dots"  ":")
+      ("blank_space"  " ")
+      ("right-straigth-parenthesis"  "[")
+      ("blank_space"  " ")
+      ("alphabet-symbol"  "a")
+      ("coma"  ",")
+      ("blank_space"  " ")
+      ("alphabet-symbol"  "b")
+      ("coma"  ",")
+      ("blank_space"  " ")
+      ("alphabet-symbol"  "0")
+      ("blank_space"  " ")
+      ("left-straigth-parenthesis"  "]")
+      ("newline"  "\n")
+      ("blank_space"  "  ")
+      ("rw-start"  "start")
+      ("dots"  ":")
+      ("blank_space"  " ")
+      ("stateId"  "q0")
+      ("newline"  "\n")
+      ("blank_space"  "  ")
+      ("rw-end"  "end")
+      ("dots"  ":")
+      ("blank_space"  " ")
+      ("stateId"  "q2")
+      ("newline"  "\n")
+      ("blank_space"  "  ")
+      ("rw-transitions"  "transitions")
+      ("blank_space"  " ")
+      ("dots"  ":")
+      ("right-straigth-parenthesis"  "[")
+      ("blank_space"  " ")
+      ("newline"  "\n")
+      ("blank_space"  "    ")
+      ("stateId"  "q0")
+      ("blank_space"  " ")
+      ("transition-sybol"  "::")
+      ("blank_space"  " ")
+      ("alphabet-symbol"  "a")
+      ("blank_space"  " ")
+      ("transition-sybol"  "::")
+      ("blank_space"  " ")
+      ("stateId"  "q1")
+      ("coma"  ",")
+      ("newline"  "\n")
+      ("blank_space"  "    ")
+      ("stateId"  "q1")
+      ("blank_space"  " ")
+      ("transition-sybol"  "::")
+      ("blank_space"  " ")
+      ("alphabet-symbol"  "b")
+      ("blank_space"  " ")
+      ("transition-sybol"  "::")
+      ("blank_space"  " ")
+      ("stateId"  "q2")
+      ("coma"  ",")
+      ("newline"  "\n")
+      ("blank_space"  "    ")
+      ("stateId"  "q2")
+      ("blank_space"  " ")
+      ("transition-sybol"  "::")
+      ("blank_space"  " ")
+      ("alphabet-symbol"  "0")
+      ("blank_space"  " ")
+      ("transition-sybol"  "::")
+      ("blank_space"  " ")
+      ("stateId"  "q0")
+      ("newline"  "\n")
+      ("blank_space"  "  ")
+      ("left-straigth-parenthesis"  "]")
+      ("newline"  "\n")
+      ("left-straigth-parenthesis"  "]")
   )
-
-  (parse-transitions-aux flat-tokens '())
 )
 
-; Parse
-(define (parse-tokens raw-tokens)
-  ; Strip whitespace tokens produced by the new Tokenizer
-  (define tokens (filter (lambda (t) (not (equal? (first t) "blank_space"))) raw-tokens))
-  ; Get all label values
-  (define (get-all label)
-    (remove-duplicates (map second (filter (lambda (t) (equal? (first t) label)) tokens)))
-  )
-  ; Get label value
-  (define (get-first label)
-    (let ([result (get-all label)])
-      (if (empty? result)
-          #f ; Return false if not found
-          (first result))))
+(define (strip-spaces raw-tokens)
+  (filter
+   (lambda (t)
+     (not (member (first t)
+                  (list "blank_space" "newline"))))
+   raw-tokens)
+)
 
-  ; Find the stateId that comes after a specific keyword
-  (define (get-state-after keyword)
-      (let ([token-list (dropf tokens (lambda (t) (not (equal? (first t) keyword))))])
-        (if (and (pair? token-list) (pair? (cdr token-list)) (equal? (first (second token-list)) "stateId"))
-            (second (second token-list))
-            #f)))
+(define (token-type tk) (first tk))
+(define (token-lexeme tk) (second tk))
 
-  ; Build Automaton
-  (list
-    (cons 'name        (get-first "identifier"))
-    (cons 'states      (get-all "stateId"))
-    (cons 'start       (get-state-after "rw-start"))
-    (cons 'end         (get-state-after "rw-end"))
-    (cons 'alphabet    (get-all "alphabet-symbol"))
-    (cons 'transitions (parse-transitions tokens))
+; Parse result accessors: (list remaining-tokens auto errors)
+(define (make-result tokens auto errors) (list tokens auto errors))
+(define (result-tokens r) (first r))
+(define (result-auto r) (second r))
+(define (result-errors r) (third r))
+
+(define (add-error errors expected received)
+  (append errors
+          (list
+           (string-append "Expected " expected ", received " received)))
+)
+
+; statesPrime ::= , stateId statesPrime | ]
+(define (syntax-statesPrime tokens auto errors)
+  (define current-token (token-type (car tokens)))
+  (cond
+    [(equal? current-token "coma")
+     (define next-token (token-type (cadr tokens)))
+     (cond
+       [(equal? next-token "stateId")
+        (define lexem (token-lexeme (cadr tokens)))
+        (define new-states (append (hash-ref auto 'states '()) (list lexem)))
+        (define new-auto (hash-set auto 'states new-states))
+        (syntax-statesPrime (cddr tokens) new-auto errors)]
+       [else (make-result tokens auto (add-error errors "stateId" next-token))])]
+    [(equal? current-token "left-straigth-parenthesis")
+     (make-result (cdr tokens) auto errors)]
+    [else (make-result tokens auto (add-error errors ", or ]" current-token))]
   )
 )
+
+; states ::= stateId statesPrime
+(define (syntax-states tokens auto errors)
+  (define current-token (token-type (car tokens)))
+  (cond
+    [(equal? current-token "stateId")
+     (define lexem (token-lexeme (car tokens)))
+     (define new-auto (hash-set auto 'states (list lexem)))
+     (syntax-statesPrime (cdr tokens) new-auto errors)]
+    [else (make-result tokens auto (add-error errors "stateId" current-token))]
+  )
+)
+
+; statesList ::= [ states
+(define (syntax-statesList tokens auto errors)
+  (define current-token (token-type (car tokens)))
+  (cond
+    [(equal? current-token "right-straigth-parenthesis")
+     (syntax-states (cdr tokens) auto errors)]
+    [else (make-result tokens auto (add-error errors "[" current-token))]
+  )
+)
+
+; statesDefinition ::= rw-states dots statesList
+(define (syntax-statesDefinition tokens auto errors)
+  (define current-token (token-type (car tokens)))
+  (cond
+    [(equal? current-token "rw-states")
+     (define next-token (token-type (cadr tokens)))
+     (cond
+       [(equal? next-token "dots")
+        (syntax-statesList (cddr tokens) auto errors)]
+       [else (make-result tokens auto (add-error errors ":" next-token))])]
+    [else (make-result tokens auto (add-error errors "states" current-token))]
+  )
+)
+
+; alphabetPrime ::= , alphabet-symbol alphabetPrime | ]
+(define (syntax-alphabetPrime tokens auto errors)
+  (define current-token (token-type (car tokens)))
+  (cond
+    [(equal? current-token "coma")
+     (define next-token (token-type (cadr tokens)))
+     (cond
+       [(equal? next-token "alphabet-symbol")
+        (define lexem (token-lexeme (cadr tokens)))
+        (define new-alpha (append (hash-ref auto 'alphabet '()) (list lexem)))
+        (define new-auto (hash-set auto 'alphabet new-alpha))
+        (syntax-alphabetPrime (cddr tokens) new-auto errors)]
+       [else (make-result tokens auto (add-error errors "alphabet-symbol" next-token))])]
+    [(equal? current-token "left-straigth-parenthesis")
+     (make-result (cdr tokens) auto errors)]
+    [else (make-result tokens auto (add-error errors ", or ]" current-token))]
+  )
+)
+
+; alphabet ::= alphabet-symbol alphabetPrime
+(define (syntax-alphabet tokens auto errors)
+  (define current-token (token-type (car tokens)))
+  (cond
+    [(equal? current-token "alphabet-symbol")
+     (define lexem (token-lexeme (car tokens)))
+     (define new-auto (hash-set auto 'alphabet (list lexem)))
+     (syntax-alphabetPrime (cdr tokens) new-auto errors)]
+    [else (make-result tokens auto (add-error errors "alphabet-symbol" current-token))]
+  )
+)
+
+; alphabetList ::= [ alphabet
+(define (syntax-alphabetList tokens auto errors)
+  (define current-token (token-type (car tokens)))
+  (cond
+    [(equal? current-token "right-straigth-parenthesis")
+     (syntax-alphabet (cdr tokens) auto errors)]
+    [else (make-result tokens auto (add-error errors "[" current-token))]
+  )
+)
+
+; alphabetDefinition ::= rw-alphabet dots alphabetList
+(define (syntax-alphabetDefinition tokens auto errors)
+  (define current-token (token-type (car tokens)))
+  (cond
+    [(equal? current-token "rw-alphabet")
+     (define next-token (token-type (cadr tokens)))
+     (cond
+       [(equal? next-token "dots")
+        (syntax-alphabetList (cddr tokens) auto errors)]
+       [else (make-result tokens auto (add-error errors ":" next-token))])]
+    [else (make-result tokens auto (add-error errors "alphabet" current-token))]
+  )
+)
+
+; startDefinition ::= rw-start dots stateId
+(define (syntax-startDefinition tokens auto errors)
+  (define current-token (token-type (car tokens)))
+  (cond
+    [(equal? current-token "rw-start")
+     (define next-token (token-type (cadr tokens)))
+     (cond
+       [(equal? next-token "dots")
+        (define state-token (token-type (caddr tokens)))
+        (cond
+          [(equal? state-token "stateId")
+           (define lexem (token-lexeme (caddr tokens)))
+           (define new-auto (hash-set auto 'start lexem))
+           (make-result (cdddr tokens) new-auto errors)]
+          [else (make-result tokens auto (add-error errors "stateId" state-token))])]
+       [else (make-result tokens auto (add-error errors ":" next-token))])]
+    [else (make-result tokens auto (add-error errors "start" current-token))]
+  )
+)
+
+; endDefinition ::= rw-end dots stateId
+(define (syntax-endDefinition tokens auto errors)
+  (define current-token (token-type (car tokens)))
+  (cond
+    [(equal? current-token "rw-end")
+     (define next-token (token-type (cadr tokens)))
+     (cond
+       [(equal? next-token "dots")
+        (define state-token (token-type (caddr tokens)))
+        (cond
+          [(equal? state-token "stateId")
+           (define lexem (token-lexeme (caddr tokens)))
+           (define new-auto (hash-set auto 'end lexem))
+           (make-result (cdddr tokens) new-auto errors)]
+          [else (make-result tokens auto (add-error errors "stateId" state-token))])]
+       [else (make-result tokens auto (add-error errors ":" next-token))])]
+    [else (make-result tokens auto (add-error errors "end" current-token))]
+  )
+)
+
+; transitionsPrime ::= , transition transitionsPrime | ]   (mutually recursive with syntax-transition)
+(define (syntax-transitionsPrime tokens auto errors)
+  (define current-token (token-type (car tokens)))
+  (cond
+    [(equal? current-token "coma")
+     (syntax-transition (cdr tokens) auto errors)]
+    [(equal? current-token "left-straigth-parenthesis")
+     (make-result (cdr tokens) auto errors)]
+    [else (make-result tokens auto (add-error errors ", or ]" current-token))]
+  )
+)
+
+; transition ::= stateId :: alphabet-symbol :: stateId  (then → transitionsPrime)
+(define (syntax-transition tokens auto errors)
+  (cond
+    [(< (length tokens) 5)
+     (make-result tokens auto (add-error errors "stateId :: alpha :: stateId" "end of input"))]
+    [else
+     (define from-type  (token-type (list-ref tokens 0)))
+     (define sym1-type  (token-type (list-ref tokens 1)))
+     (define alpha-type (token-type (list-ref tokens 2)))
+     (define sym2-type  (token-type (list-ref tokens 3)))
+     (define to-type    (token-type (list-ref tokens 4)))
+     (cond
+       [(and (equal? from-type  "stateId")
+             (equal? sym1-type  "transition-sybol")
+             (equal? alpha-type "alphabet-symbol")
+             (equal? sym2-type  "transition-sybol")
+             (equal? to-type    "stateId"))
+        (define from  (token-lexeme (list-ref tokens 0)))
+        (define alpha (token-lexeme (list-ref tokens 2)))
+        (define to    (token-lexeme (list-ref tokens 4)))
+        (define old-tr   (hash-ref auto 'transitions (hash)))
+        (define from-map (hash-ref old-tr from (hash)))
+        (define new-auto (hash-set auto 'transitions
+                                   (hash-set old-tr from (hash-set from-map alpha to))))
+        (syntax-transitionsPrime (list-tail tokens 5) new-auto errors)]
+       [else (make-result tokens auto (add-error errors "stateId :: alpha :: stateId" from-type))])]
+  )
+)
+
+; transitionsList ::= [ transition transitionsPrime
+(define (syntax-transitionsList tokens auto errors)
+  (define current-token (token-type (car tokens)))
+  (cond
+    [(equal? current-token "right-straigth-parenthesis")
+     (syntax-transition (cdr tokens) auto errors)]
+    [else (make-result tokens auto (add-error errors "[" current-token))]
+  )
+)
+
+; transitionsDefinition ::= rw-transitions dots transitionsList
+(define (syntax-transitionsDefinition tokens auto errors)
+  (define current-token (token-type (car tokens)))
+  (cond
+    [(equal? current-token "rw-transitions")
+     (define next-token (token-type (cadr tokens)))
+     (cond
+       [(equal? next-token "dots")
+        (syntax-transitionsList (cddr tokens) auto errors)]
+       [else (make-result tokens auto (add-error errors ":" next-token))])]
+    [else (make-result tokens auto (add-error errors "transitions" current-token))]
+  )
+)
+
+; automaton ::= rw-automata identifier [ statesDefinition alphabetDefinition startDefinition endDefinition transitionsDefinition ]
+(define (syntax-automaton tokens auto errors)
+  (define kw-type (token-type (car tokens)))
+  (cond
+    [(equal? kw-type "rw-automata")
+     (define id-type (token-type (cadr tokens)))
+     (cond
+       [(equal? id-type "identifier")
+        (define bracket-type (token-type (caddr tokens)))
+        (cond
+          [(equal? bracket-type "right-straigth-parenthesis")
+           (define name (token-lexeme (cadr tokens)))
+           (define auto+name (hash-set auto 'name name))
+           (define states-r (syntax-statesDefinition     (cdddr tokens)             auto+name            errors))
+           (define alpha-r  (syntax-alphabetDefinition   (result-tokens states-r)   (result-auto states-r)  (result-errors states-r)))
+           (define start-r  (syntax-startDefinition      (result-tokens alpha-r)    (result-auto alpha-r)   (result-errors alpha-r)))
+           (define end-r    (syntax-endDefinition        (result-tokens start-r)    (result-auto start-r)   (result-errors start-r)))
+           (define trans-r  (syntax-transitionsDefinition (result-tokens end-r)     (result-auto end-r)     (result-errors end-r)))
+           (define rem        (result-tokens trans-r))
+           (define fin-auto   (result-auto   trans-r))
+           (define fin-errors (result-errors trans-r))
+           (cond
+             [(and (not (null? rem))
+                   (equal? (token-type (car rem)) "left-straigth-parenthesis"))
+              (make-result (cdr rem) fin-auto fin-errors)]
+             [else
+              (define got (if (null? rem) "EOF" (token-type (car rem))))
+              (make-result rem fin-auto (add-error fin-errors "]" got))])]
+          [else (make-result tokens auto (add-error errors "[" bracket-type))])]
+       [else (make-result tokens auto (add-error errors "identifier" id-type))])]
+    [else (make-result tokens auto (add-error errors "Automaton" kw-type))]
+  )
+)
+
+; Recursive descent entry point
+(define (Recursive-descent stripped-tokens)
+  (define result   (syntax-automaton stripped-tokens (hash) '()))
+  (define errors   (result-errors result))
+  (define auto     (result-auto   result))
+  (cond
+    [(null? errors)
+     (displayln "Parsing successful!")
+     (displayln "Automaton:")
+     (displayln auto)
+     (displayln "Transitions:")
+     (displayln (hash-ref auto 'transitions (hash)))]
+    [else
+     (displayln "Parsing failed with errors:")
+     (for-each displayln errors)])
+)
+
+(Recursive-descent (strip-spaces raw-tokens))
